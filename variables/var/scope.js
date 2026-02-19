25431ef9c863e5f13a7842c90194d5ac9a735385
Harsh <harshvishwakarma5071@gmail.com>
2026-02-19 14:27:14 +0530

    Update scope.js

--- variables/var/scope.js
+++ variables/var/scope.js
@@ -1,6 +1,13 @@
 // var is Function scoped.
 // unlike normal variables, var ignores the if or for block and its scope is the entire function
 // Example:
+var x = 1;
+if (x === 1) {
+  var x = 2;
+  console.log(x);
+}
+console.log(x);
+
 function testScope() {
   if (true) {
     var x = "I am function scoped";
